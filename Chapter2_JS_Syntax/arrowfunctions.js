3589974d19faac47f1727c3ea0b6d6ca09cde2d4
68Kamil68 <tret1lus@gmail.com>
2020-02-13 22:57:43 +0100

    simple js syntax

---
+++ Chapter2_JS_Syntax/arrowfunctions.js
@@ -0,0 +1,13 @@
+function printMyName(name) {
+    console.log(name);
+}
+
+printMyName('Kamil');
+
+const printMySurname = surname => {
+    console.log(surname);
+} 
+
+const multiply = number => number * 2;
+
+console.log(multiply(2));
---
+++ Chapter2_JS_Syntax/letconst.js
@@ -0,0 +1,11 @@
+var myName = 'Kamil'
+console.log(myName);
+
+myName = 'Maro';
+console.log(myName);
+
+const mySurname = 'Kamilek'
+console.log(mySurname);
+
+//mySurname = 'Maro';
+console.log(mySurname);
