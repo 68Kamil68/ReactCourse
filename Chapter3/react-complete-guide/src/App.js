0efb400dc12d3e30838a96b0b474d177e674954f
Kamil Krakowski <tret1lus@gmail.com>
2020-02-14 17:11:02 +0100

    react basics

--- Chapter3/react-complete-guide/src/App.js
+++ Chapter3/react-complete-guide/src/App.js
@@ -1,19 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  render() {
+const app = props => {
+  const [ personsState, setPersonsState] = useState({
+    persons: [
+      { name: 'Kamil', age: 23},
+      { name: 'Jan', age: 21},
+      { name: 'Grzes', age: 30}
+    ],
+  });
+  const [otherState, setOtherState] = useState('some other value');
+  
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    setPersonsState({persons: [
+      { name: 'Kamilek', age: 23},
+      { name: 'Jan', age: 21},
+      { name: 'Grzes', age: 32}
+    ]
+  });
+  }
     return (
       <div className="App">
         <h1>Hi, I'm a react app</h1>
         <p>This is really working</p>
-        <Person />
-        <Person />
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobbies: racing</Person>
         <Person />
       </div>
     );
-  }
 }
 
-export default App;
+export default app;
